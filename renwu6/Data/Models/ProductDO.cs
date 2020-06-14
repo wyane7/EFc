3ef0685d78edbb68b9858fc76727e988b0aa7fcb
xing-yu-chen <17367288870@sina.cn>
2020-06-14 14:28:45 +0800

    666

---
+++ renwu6/Data/Models/ProductDO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Data.Models
+{
+    [Table("Products")]
+    class ProductDO
+    {
+        [Key] public long Id { get; set; }
+        //顾客
+        [StringLength(100)] public string Customer { get; set; }
+
+        //管理员
+        [StringLength(100)] public string Admin { get; set; }
+
+        //商品
+        [StringLength(100)] public string Commodity { get; set; }
+
+        //分类
+        [StringLength(100)] public string Classification  { get; set; }
+
+        //订单
+        [StringLength(100)] public string Order  { get; set; }
+
+    }
+}
---
+++ renwu6/TinyShop/Migrations/20200614034411_initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TinyShop.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Customer = table.Column<string>(maxLength: 100, nullable: true),
+                    Admin = table.Column<string>(maxLength: 100, nullable: true),
+                    Commodity = table.Column<string>(maxLength: 100, nullable: true),
+                    Classification = table.Column<string>(maxLength: 100, nullable: true),
+                    Order = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
